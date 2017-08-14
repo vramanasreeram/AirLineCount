73b2709b6e64f7ad0db97e18553aa3ba6d7597f3
vramanasreeram <vramanasreeram@gmail.com>
2017-08-14 13:30:59 +0530

    Required string BHM

--- src/main/java/org/tweaktalent/hadoop/airline/AirLineMap.java
+++ src/main/java/org/tweaktalent/hadoop/airline/AirLineMap.java
@@ -14,7 +14,7 @@ public class AirLineMap extends Mapper<LongWritable,Text,Text,IntWritable>{
 		
 		String[] words = value.toString().split(",");
 		if(words[0].equalsIgnoreCase("2008")) {
-			if(words[17].equalsIgnoreCase("BWI"))
+			if(words[17].equalsIgnoreCase("BHM"))
 			{
 			context.write(new Text(words[17]),new IntWritable(1));
 		}
