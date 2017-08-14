e2c2857052e1a690adfcd5ad403419ae7eb45418
vramanasreeram <vramanasreeram@gmail.com>
2017-08-14 11:52:38 +0530

    required string

--- src/main/java/org/tweaktalent/hadoop/airline/AirLineMap.java
+++ src/main/java/org/tweaktalent/hadoop/airline/AirLineMap.java
@@ -9,11 +9,15 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 public class AirLineMap extends Mapper<LongWritable,Text,Text,IntWritable>{
 
+	
 	public void map(LongWritable key,Text value,Context context) throws IOException, InterruptedException {
 		
 		String[] words = value.toString().split(",");
 		if(words[0].equalsIgnoreCase("2008")) {
+			if(words[17].equalsIgnoreCase("BWI"))
+			{
 			context.write(new Text(words[17]),new IntWritable(1));
+		}
 	}
 	}
 }
--- src/main/java/org/tweaktalent/hadoop/airline/AirLineReduce.java
+++ src/main/java/org/tweaktalent/hadoop/airline/AirLineReduce.java
@@ -5,16 +5,21 @@ import java.io.IOException;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.log4j.Logger;
+
 
 public class AirLineReduce extends Reducer<Text,IntWritable,Text,IntWritable> {
 
+	Logger logger = Logger.getLogger(AirLineReduce.class);
 	public void reduce(Text key,Iterable<IntWritable> values,Context context) throws IOException, InterruptedException {
 		
 		int sum =0;
 		for(IntWritable value:values) {
-			System.out.println("Key:"+ key.toString() + " Values:"+value.get());
+//			System.out.println("Key:"+ key.toString() + " Values:"+value.get());
+			logger.info("Key:"+ key.toString() + " Values:"+value.get());
 			sum+= value.get();
 		}
 		context.write(key, new IntWritable(sum));
 	}
 }
+
