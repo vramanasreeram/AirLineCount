5fca0d5e13c169f03c53fe51ffb545dc790ee512
vramanasreeram <vramanasreeram@gmail.com>
2017-08-14 07:36:13 +0530

    with sysout

--- src/main/java/org/tweaktalent/hadoop/airline/AirLineReduce.java
+++ src/main/java/org/tweaktalent/hadoop/airline/AirLineReduce.java
@@ -9,6 +9,7 @@ import org.apache.hadoop.mapreduce.Reducer;
 public class AirLineReduce extends Reducer<Text,IntWritable,Text,IntWritable> {
 
 	public void reduce(Text key,Iterable<IntWritable> values,Context context) throws IOException, InterruptedException {
+		System.out.println("Key:"+ key.toString() + " Values:"+values.toString());
 		int sum =0;
 		for(IntWritable value:values) {
 			sum+= value.get();
