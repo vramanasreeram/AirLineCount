2850a0df6add7eea2fac5e65e6621260e4d37a4a
vramanasreeram <vramanasreeram@gmail.com>
2017-08-17 00:21:17 +0530

    changed logic for inputtextfile

--- src/main/java/org/tweaktalent/hadoop/airline/AirLineMap.java
+++ src/main/java/org/tweaktalent/hadoop/airline/AirLineMap.java
@@ -1,6 +1,7 @@
 package org.tweaktalent.hadoop.airline;
 
 import java.io.IOException;
+import java.util.StringTokenizer;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
@@ -9,16 +10,25 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 public class AirLineMap extends Mapper<IntWritable,Text,Text,IntWritable>{
 
+	private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
 	
 	//public void map(LongWritable key,Text value,Context context) throws IOException, InterruptedException {
 	public void map(IntWritable key,Text value,Context context) throws IOException, InterruptedException {
 		
-		String[] words = value.toString().split(",");
+		//String[] words = value.toString().split(",");
 		//if(words[0].equalsIgnoreCase("2008")) {
 			//if(words[17].equalsIgnoreCase("BHM")|| words[17].equalsIgnoreCase("BNA")||words[17].equalsIgnoreCase("cec"))
 			//{
-			context.write(new Text(words[0]),new IntWritable(1));
+			//context.write(new Text(words[0]),new IntWritable(1));
 		//}
 	//}
+		
+		String line = value.toString();
+        StringTokenizer tokenizer = new StringTokenizer(line);
+        while (tokenizer.hasMoreTokens()) {
+            word.set(tokenizer.nextToken());
+            context.write(word, one);
+        }
 	}
 }
